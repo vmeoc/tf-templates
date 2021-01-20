fe8962a38c17e6c82bd52412a16a1b4e79780b87
vincent.meoc@gmail.com <vincent.meoc@gmail.com>
2021-01-20 12:01:44 +0100

    cleanup credentials

--- Azure-Nw/main.tf
+++ Azure-Nw/main.tf
@@ -3,11 +3,6 @@ provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
 #  version = "=2.4.0"
 
-  subscription_id = "927c2711-85d7-46f5-9e65-ff653e1145c1"
-  client_id       = "7fbfe5da-2d38-412b-b592-4ebc51103430"
-  client_secret   = var.client_secret
-  tenant_id       = "b39138ca-3cee-4b4a-a4d6-cd83d9dd62f0"
-
   features {}
 
 }
