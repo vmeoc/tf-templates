db361996c342c4d5212a86738a6e154b1b708844
vincent.meoc@gmail.com <vincent.meoc@gmail.com>
2021-04-06 20:01:04 +0200

    added subnet

--- Azure-Nw/main.tf
+++ Azure-Nw/main.tf
@@ -19,3 +19,11 @@ resource "azurerm_virtual_network" "example" {
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
 }
+
+# Create a virtual subnet within the virtual nw
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefix     = "10.0.1.0/24"
+}
