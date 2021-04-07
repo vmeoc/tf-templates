e1dcf99d5eb8270a1afb1b25f8edd211ea433200
vincent.meoc@gmail.com <vincent.meoc@gmail.com>
2021-04-07 12:07:46 +0200

    fixed vra provider issue

--- vRANetwProf/main.tf
+++ vRANetwProf/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    vra = {
+      source = "vmware/vra"
+      version = "0.3.5"
+    }
+  }
+}
 provider vra {
   url           = var.url
   refresh_token = var.refresh_token
