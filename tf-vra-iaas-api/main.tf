61c25e45482ff7d7ebb36038a3c3e245a4d81c4f
vincent.meoc@gmail.com <vincent.meoc@gmail.com>
2021-11-16 13:51:46 +0100

    VM deployment OK

--- tf-vra-iaas-api/main.tf
+++ tf-vra-iaas-api/main.tf
@@ -4,9 +4,6 @@ provider "vra" {
   insecure      = true
 }
 
-data "vra_cloud_account_vsphere" "this" {
-  name = var.cloud_account
-}
 
 data "vra_project" "this" {
   name = var.project
@@ -16,16 +13,6 @@ data "vra_network" "this" {
   name = var.network_name
 }
 
-#resource "vra_block_device" "disk1" {
-#  capacity_in_gb = 10
-#  name = "terraform_vra_block_device1"
-#  project_id = data.vra_project.this.id
-#  constraints {
-#    mandatory  = true
-#    expression = "cpod:vrealize"
-#  }
-
-#}
 
 resource "random_id" "id" {
 	  byte_length = 8
@@ -43,18 +30,11 @@ resource "vra_machine" "this" {
   boot_config {
     content = <<EOF
 #cloud-config
-  users:
-  - default
-  - name: myuser
-    sudo: ['ALL=(ALL) NOPASSWD:ALL']
-    groups: [wheel, sudo, admin]
-    shell: '/bin/bash'
-    ssh-authorized-keys: |
-      ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDytVL+Q6/UmGwdnJxKQEozqERHGqxlH/zBbT5W8iNbwgOLF6JWz0o7ThAK/Cf0uPcv78Q6UhOjuRfd2BKBciJx5JsyH4Ly7Ars2v/ZQ492KyZElKRqwibXNWjfZcwKU/6YjDITm15Yh6UWCsvVHg4w72X+TiTxeKDZ0pNt2hcZ5Uje6NvZ4GFKYfl4kNFxBZmBYLFdtq8eNPg3PGREV+pM0xkyXKSAYUsXsgj821AgK/YNByCPY53jNKqXqdFKQXKG7FOs78MdhAF7aGMsVRymY5RtHk9UO0DGzCIHRp9DqmfN9SdIYIf5fb4sEtt8T9uxW32Mx3d9S+vGbmkYoRpY user@example.com
-
   runcmd:
-    - sudo sed -e 's/.*PasswordAuthentication yes.*/PasswordAuthentication no/' -i /etc/ssh/sshd_config
-    - sudo service sshd restart
+    - yum install httpd -y
+    - systemctl enable httpd
+    - systemctl start httpd
+    - echo V2 >> /var/www/html/index.html
 EOF
   }
 
@@ -66,19 +46,12 @@ EOF
 
   }
 
-#  constraints {
-#    mandatory  = true
-#    expression = "cpod:cpod-vr"
-#  }
 
   tags {
     key   = "foo"
     value = "bar"
   }
 
-#  disks {
-#    block_device_id = vra_block_device.disk1.id
-#  }
 
   timeouts {
     create = "20m"
@@ -86,3 +59,6 @@ EOF
 
 }
 
+output "machine_ip_addr" {
+    value = vra_machine.this.address
+}
