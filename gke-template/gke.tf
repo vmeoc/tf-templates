b63aabd23f4de22bfb94bc80b3a5dfb548f01825
vincent.meoc@gmail.com <vincent.meoc@gmail.com>
2022-06-14 22:07:54 +0200

    reduce number of node to 1 to cope with free tier quota. Works OK

--- gke-template/gke.tf
+++ gke-template/gke.tf
@@ -9,7 +9,7 @@ variable "gke_password" {
 }
 
 variable "gke_num_nodes" {
-  default     = 2
+  default     = 1
   description = "number of gke nodes"
 }
 
@@ -22,7 +22,7 @@ resource "google_container_cluster" "primary" {
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
-  initial_node_count       = 2
+  initial_node_count       = 1
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
